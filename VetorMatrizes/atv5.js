32d6c5f751ca223d88d3f87704e4e9e595fc56b6
Suyanne Miranda <66568437+Suyannesara@users.noreply.github.com>
2022-06-17 11:21:24 -0300

    Finishing arrays lists

--- VetorMatrizes/atv5.js
+++ VetorMatrizes/atv5.js
@@ -1,1 +1,27 @@
 //Faça um programa que crie e preencha uma matriz  A [3 x 3] com números reais. Após o preenchimento da matriz, alimente uma segunda matriz B, de mesma dimensão, com os mesmos valores da matriz A multiplicados por 2. Ao final apresente a matriz B.
+
+let A = [];
+let B = [];
+
+//Prencher linha
+for (let i = 0; i < 3; i++) {
+  //Prencher coluna
+  A[i] = [];
+
+  for (let j = 0; j < 3; j++) {
+    A[i][j] = Number(
+      prompt(`Digite o número que deseja guardar em ${i}, ${j}: `)
+    );
+  }
+}
+
+for (i = 0; i <= 3; i++) {
+  B[i] = [];
+
+  for (j = 0; j < 3; j++) {
+    B[i][j] = A[i][j] * 2;
+  }
+}
+
+console.log(A);
+console.log(B);
--- VetorMatrizes/atv7.js
+++ VetorMatrizes/atv7.js
@@ -1,1 +1,23 @@
 //7) Faça um programa que crie e preencha uma matriz  A [5 x 5] com números reais. Ao final apresente os valores armazenados nas linhas 1 e 5.
+
+let A = [];
+
+//Prencher linha
+for (let i = 0; i < 5; i++) {
+  //Prencher coluna
+  A[i] = [];
+
+  for (let j = 0; j < 5; j++) {
+    A[i][j] = parseFloat(
+      prompt(`Digite o número REAL que deseja guardar em ${i}, ${j}: `)
+    );
+  }
+}
+
+for (let i = 0; i < 5; i++) {
+
+    for (let j = 0; j < 5; j + 4) {
+        console.log(`Coluna ${i}: ${A[i][j]}`);
+    }
+}
+
